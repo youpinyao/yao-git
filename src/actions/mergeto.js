68f52fa1cb7678331ff27277d428ca84642b2cc6
游品尧 <497400448@qq.com>
2018-06-26 12:01:04 +0800

    build[2018-6-26 12:01:04]:build

--- src/actions/mergeto.js
+++ src/actions/mergeto.js
@@ -2,7 +2,7 @@
 // const path = require('path');
 const chalk = require('chalk');
 const spawn = require('cross-spawn');
-// const exec = require('child_process').exec;
+const exec = require('child_process').exec;
 
 function doSpawnSync(cmd, args) {
   const options = {
@@ -18,15 +18,15 @@ function doSpawnSync(cmd, args) {
 
 module.exports = (branch) => {
   process.chdir(process.cwd());
-  const currentBranch = doSpawnSync('git', ['symbolic-ref', '--short', '-q', 'HEAD']);
-
-  doSpawnSync('git', ['pull']);
-  doSpawnSync('git', ['add', './']);
-  doSpawnSync('git', ['commit', '-m', `build[${(new Date()).toLocaleString()}]:build`]);
-  doSpawnSync('git', ['push']);
-  doSpawnSync('git', ['checkout', branch]);
-  doSpawnSync('git', ['pull']);
-  doSpawnSync('git', ['merge', currentBranch, '-m', `merge[${(new Date()).toLocaleString()}]: ${currentBranch} merge to ${branch}`]);
-  doSpawnSync('git', ['push']);
-  doSpawnSync('git', ['checkout', currentBranch]);
+  exec('git symbolic-ref --short -q HEAD', (err, currentBranch) => {
+    doSpawnSync('git', ['pull']);
+    doSpawnSync('git', ['add', './']);
+    doSpawnSync('git', ['commit', '-m', `build[${(new Date()).toLocaleString()}]:build`]);
+    doSpawnSync('git', ['push']);
+    doSpawnSync('git', ['checkout', branch]);
+    doSpawnSync('git', ['pull']);
+    doSpawnSync('git', ['merge', currentBranch, '-m', `merge[${(new Date()).toLocaleString()}]: ${currentBranch} merge to ${branch}`]);
+    doSpawnSync('git', ['push']);
+    doSpawnSync('git', ['checkout', currentBranch]);
+  });
 };
